641b460a140bd246650e2b9351ab62ba185997db
naaz67 <167643849+naaz67@users.noreply.github.com>
2024-04-26 21:14:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Ainulk6/DAXXMUSIC",
+    "https://github.com/naaz67/DAXXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
